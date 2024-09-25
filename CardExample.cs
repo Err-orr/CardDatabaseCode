f69af7515415a7b904d9c091c53836acd28e9587
Err-orr <131775895+Err-orr@users.noreply.github.com>
2024-09-25 08:47:58 -0400

    Create CardExample.cs

---
+++ CardExample.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDatabase : MonoBehaviour
+{
+    public static List<Card> cardList = new List<Card>();
+
+    void Awake()
+    {
+        
+    }
+
+}
