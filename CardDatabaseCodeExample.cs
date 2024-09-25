3b4944b08f720eb7251374dc7cf6499b15dd74b6
Err-orr <131775895+Err-orr@users.noreply.github.com>
2024-09-25 08:48:51 -0400

    Update and rename CardExample.cs to CardDatabaseCodeExample.cs
    Incorrect file name

--- CardDatabaseCodeExample.cs
+++ CardDatabaseCodeExample.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardDatabase : MonoBehaviour
+public class CardDatabaseCodeExample : MonoBehaviour
 {
     public static List<Card> cardList = new List<Card>();
 
